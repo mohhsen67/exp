886910514522e730acb311ec865208670805bba8
Mohsen Jafari <mohhsen67@gmail.com>
2023-04-16 20:11:31 +0200

    Defined the ExpController.

--- src/main/java/com/mohhsen/exp/expenses/ExpController.java
+++ src/main/java/com/mohhsen/exp/expenses/ExpController.java
@@ -1,5 +1,12 @@
 package com.mohhsen.exp.expenses;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ExpController {
-    
+    @GetMapping("/")
+	public String addCategory() {
+		return "Added new category";
+	}
 }
